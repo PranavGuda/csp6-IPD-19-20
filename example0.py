1323594597a420fb12451d98ad2fae16f374f3d4
PranavGuda <guda6922@mydusd.org>
2020-02-19 21:33:10 +0000

    I added three strategies

--- example0.py
+++ example0.py
@@ -10,15 +10,8 @@ team_name = 'E0'
 strategy_name = 'Collude unless losing or tied'
 strategy_description = 'Always collude unless you are losing or tied, then betray'
     
-def move(my_history, their_history, my_score, their_score):
-    '''Make my move based on the history with this player.
-    
-    history: a string with one letter (c or b) per round that has been played with this opponent.
-    their_history: a string of the same length as history, possibly empty. 
-    The first round between these two players is my_history[0] and their_history[0]
-    The most recent round is my_history[-1] and their_history[-1]
-    
-    Returns 'c' or 'b' for collude or betray.
+def move( my_score, their_score):
+    '''makes move based on the comparison of my score to their score
     '''
     if my_score <= their_score:
       return 'b'
--- example3.py
+++ example3.py
@@ -10,15 +10,8 @@ team_name = 'E3'
 strategy_name = 'retaliate but collude'
 strategy_description = '''betray the first round and always betray unless they colluded in the previous round'''
     
-def move(my_history, their_history, my_score, their_score):
+def move(my_history, their_history):
     '''Make my move based on the history with this player.
-    
-    history: a string with one letter (c or b) per round that has been played with this opponent.
-    their_history: a string of the same length as history, possibly empty. 
-    The first round between these two players is my_history[0] and their_history[0]
-    The most recent round is my_history[-1] and their_history[-1]
-    
-    Returns 'c' or 'b' for collude or betray.
     '''
     if len(my_history)==0: # It's the first round; betray.
         return 'b'
